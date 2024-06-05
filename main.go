2f46cf922eb91410c1a44c5fc3e678c379e5314d
CorvusCoraxHD <80818526+CorvusCoraxHD@users.noreply.github.com>
2024-06-06 00:48:59 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,36 @@
-// main.go
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, World!")
-}
-
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, world of go!")
+	})
+
+	log.Println("Starting server...")
+	l, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	go func() {
+		log.Fatal(http.Serve(l, nil))
+	}()
+
+	log.Println("Sending request...")
+	res, err := http.Get("http://localhost:8080/hello")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Reading response...")
+	if _, err := io.Copy(os.Stdout, res.Body); err != nil {
+		log.Fatal(err)
+	}
 }
